53e70f479b9d85b3984e26b4e3f339aa756c1e30
jkania7 <jkania@alumni.cmu.edu>
2018-03-22 20:06:48 -0400

    updated ploting for g031

--- g031.py
+++ g031.py
@@ -6,34 +6,43 @@ from scipy.optimize import curve_fit
 from matplotlib import rc
 rc('font',**{'family':'sans-serif','sans-serif':['Computer Modern Roman','Helvetica']})#makes the plots have pretty fonts
 rc('text', usetex=True)
+plt.rcParams.update({'font.size':11})
+def sci_not(v,err,rnd=2):#addapted from https://stackoverflow.com/questions/17088510/is-there-a-python-module-that-convert-a-value-and-an-error-to-a-scientific-notat
+    power =  - int(('%E' % v)[-3:])
+    rnd = - power - int(('%E' % float("{0:.2g}".format(err)))[-3:])
+    return r"({0} \pm {1})E({2})".format(
+            round(v*10**power,rnd+1),round(err*10**power,rnd+1),-power)
 
 r = urllib2.urlopen('http://astro.phys.wvu.edu/hii/G031.727.dat')#gets the data
 g031 = np.genfromtxt(r,delimiter=' ')#parses the data
 print('shape(g031)={0}'.format(g031.shape))
 g031 = g031[0:4096-550,:]#cut off bad parts of passband, found via trial and error
 print('shape(g031\')={0}'.format(g031.shape))#makes sure that the cut array is the correct size
+#g031=g031.astype(np.float32)
+#print(g031.dtype)
+g031[:,0] *= 10.0**(-3.0)
 """
 plt.plot(g031[:,0], g031[:,1],label="Antenna Temperature")
 plt.title(r"Determining Telescope Beamwidth")
 plt.ylabel(r"Antenna Temperature [K]")
-plt.xlabel(r"Frequency [Hz]")
+plt.xlabel(r"Frequency [GHz]")
 plt.show()"""
 #first look at data, uncomment to see
 
 def guassian(x, a, b, c, p, s, m):#defines the guassian function
     return a*np.exp(-1.0*(x-b)**2.0/(2.0*c**2.0)) + p*x**2.0 + s*x + m
-a0 = max(g031[:,1]) #Guess for A
-b0 = g031[np.argmax(g031[:,1]),0]+0.3#look at where that A occured,0.3 is added so it fitts the correct hump 
-c0 = 0.01 #guess from visual inspection
+a0 = max(g031[:,1])- np.mean(g031[:,1])#Guess for A
+b0 = g031[np.argmax(g031[:,1]),0]#look at where that A occured,0.3 is added so it fitts the correct hump 
+c0 = 0.001 #guess from visual inspection
 m0 = np.mean(g031[:,1])#guess for the mean
-s0 = 0.5 #guess for the slope, found visually
-p0 = 0.2 #guess for polynomial, found visually
+s0 = 0.37 #guess for the slope, found visually
+p0 = 0.05 #guess for polynomial, found visually
 print('a0={0}'.format(a0))
-print('b0={0}'.format(b0))
-print('c0={0}'.format(c0))
+print('v0={0}'.format(b0))
+print('sigma0={0}'.format(c0))
 print('p0={0}'.format(p0))
-print('s0={0}'.format(s0))
-print('m0={0}'.format(m0))
+print('m0={0}'.format(s0))
+print('b0={0}'.format(m0))
 
 popt,pcov = curve_fit(guassian, g031[:,0], g031[:,1],p0=[a0,b0,c0,p0,s0,m0])
 #fits the guassian,p0 is inital guess for variables
@@ -45,11 +54,12 @@ ydata = guassian(xdata,*popt)
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(111)
 ax1.scatter(g031[:,0], g031[:,1],marker="s",color='r',label='Observed',s=1)
-ax1.plot(xdata,ydata,label="Best Fit Guassian\n $a={0:.2e}\pm{1:.1e}K$\n$\\nu_0={2:.3f}\pm{3:.3f} Hz$\n$\sigma={4:.3f}\pm{5:.3f}Hz$\n$p={6:.2e}\pm{7:.1e}K/Hz^2$\n$m={8:.4f}\pm{9:.4f}K/Hz$\n$b={10:d}\pm{11:2d}K$".\
-             format(popt[0],perr[0],popt[1],perr[1],popt[2],perr[2],popt[3],perr[3],popt[4],perr[4],int(round(popt[5],-2)),int(perr[5])))
+ax1.plot(xdata,ydata,label="Best Fit Guassian\n $a={0}K$\n$\\nu_0={1} Hz$\n$\sigma={2}Hz$\n$p={3}K/Hz^2$\n$m={4}K/Hz$\n$b={5}K$".\
+             format(sci_not(popt[0],perr[0]),sci_not(popt[1],perr[1]),sci_not(popt[2],perr[2]),sci_not(popt[3],perr[3]),sci_not(popt[4],perr[4]),sci_not(int(popt[5]),int(perr[5]))))
 plt.title(r"Fitting Spectral Line with  Guassian, $g(\nu) = a\exp\Big[{-\frac{(\nu-\nu_0)^2}{2\sigma^2}}\Big]+p\nu^2+m\nu+b$")
 plt.ylabel(r"Antenna Temperature [K]")
-plt.xlabel(r"$\nu$ - Frequency [Hz]")
-plt.legend(loc='best')
+plt.xlabel(r"$\nu$ [GHz]")
+plt.legend(loc='lower right')
+plt.autoscale(enable=True, axis='x', tight=True)
 plt.savefig('G031.pdf', bbox_inches='tight')#saves the output pdf
 plt.show() #shows plot
